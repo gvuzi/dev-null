a8d466bff891616eb7cac573e8e9f7f0ce3a9368
gvuzi <gideoncallagan@gmail.com>
2025-03-26 14:00:57 -0500

    added basic hub scene & basic player movement system

---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController characterController;
+    public float speed = 5f;
+
+    void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 direction) {
+        characterController.Move(direction * speed * Time.deltaTime);
+        transform.LookAt(transform.position + direction);
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Player player;
+    public Transform cameraTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        Vector3 cameraForward = cameraTransform.forward;
+        cameraForward.y = 0;
+
+        Vector3 cameraRight = cameraTransform.right;
+        cameraRight.y = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += cameraForward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement -= cameraForward;
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            movement -= cameraRight;
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            movement += cameraRight;
+        }
+
+        movement.Normalize();
+        player.Move(movement);
+    }
+}
