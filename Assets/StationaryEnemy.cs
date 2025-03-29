b4718cc98c6251f64d97361e7bc147fdb7fdd9aa
gvuzi <gideoncallagan@gmail.com>
2025-03-28 20:15:41 -0500

    implemented leading player target with enemy projectiles

--- Assets/StationaryEnemy.cs
+++ Assets/StationaryEnemy.cs
@@ -11,48 +11,75 @@ public class StationaryEnemy : MonoBehaviour
     public GameObject bulletPrefab;
     public float bulletSpeed = 45f;
     public float shootTime = 1f;
+    public float shootCooldown = 2f;
     public Transform startPoint;
-    private bool canShoot = true;
+
+    // velocity variables
+    Vector3 playerVelocity;
+    Vector3 playerLastPosition;
+    Vector3 playerCurrentPosition;
+   
     void Start()
     {
-        
+        playerLastPosition = player.position;
+        InvokeRepeating("Shoot", 0f, shootCooldown);
     }
 
     void Update()
     {
-        Shoot();
+        //calculate player's velocity
+        playerCurrentPosition = player.position;
+        playerVelocity = (playerCurrentPosition - playerLastPosition) / Time.deltaTime;
+        playerLastPosition = playerCurrentPosition;
+        
     }
 
     void Shoot() {
-        if(Vector3.Distance(enemy.transform.position, player.transform.position) > 10) {
+        if(Vector3.Distance(enemy.transform.position, player.transform.position) > 20) {
             return;
         }
+
         transform.LookAt(player.position);
 
-        if(canShoot) {
-        Vector3 endPoint = player.transform.position;
+       
+        Vector3 endPoint = CalculateEndPoint();
         GameObject bullet = Instantiate(bulletPrefab, startPoint.position, Quaternion.identity);
         StartCoroutine(ShootRoutine(bullet, endPoint));
-        }
+        
+    }
+
+    Vector3 CalculateEndPoint() {
+        // calculate distance from spawn point to player
+        float distance = Vector3.Distance(startPoint.position, player.position);
+
+        // calculate bullet fly time, time it takes to reach player's pos
+        float bulletFlyTime = distance / bulletSpeed;
+
+        // calculates end point (where player will be)
+        Vector3 endPoint = player.position + (playerVelocity * bulletFlyTime);
+
+        // extend bullet path past the player
+        float extendedDistance = 70f;
+        Vector3 direction = (endPoint - startPoint.position).normalized;
+        Vector3 extendedEndPoint = endPoint + (direction * extendedDistance);
+        return extendedEndPoint;
     }
 
     IEnumerator ShootRoutine(GameObject bullet, Vector3 endPoint) {
+       
         float distance = Vector3.Distance(startPoint.position, endPoint);
         float shootTime = distance / bulletSpeed;
 
         float t = 0;
 
-        canShoot = false;
-        yield return new WaitForSeconds(1f);
-        canShoot = true;
 
         while(t < shootTime) {
             t += Time.deltaTime;
             bullet.transform.position = Vector3.Lerp(startPoint.position,endPoint,t/shootTime);
             yield return null;
         }
-        bullet.transform.position = endPoint;
         Destroy(bullet, 1f);
+ 
         yield return null;
     }
 
