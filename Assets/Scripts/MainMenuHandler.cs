cb07964c20a92be188d0bbd245a2eeafefacb420
gvuzi <gideoncallagan@gmail.com>
2025-05-03 18:01:40 -0500

    sound implementation for main menu

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,18 +1,47 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class MainMenuHandler : MonoBehaviour
 {
+
+    public AudioSource audioSource;
+    public AudioClip clickSound;
+    public Button startButton;
+    public Button optionsButton;
+    public Button quitButton;
+
+
     public void StartGame() {
-        SceneManager.LoadScene("Hub-Dorm");
+        StartCoroutine(AudioRoutine("Hub-Dorm", startButton));
     }
 
     public void OptionsMenu() {
-        SceneManager.LoadScene("Options-Menu");
+        StartCoroutine(AudioRoutine("Options-Menu", optionsButton));
     }
 
     public void QuitGame() {
+        StartCoroutine(AudioRoutine(null, quitButton));
+    }
+
+    IEnumerator AudioRoutine(string scene, Button button) {
+        button.interactable = false;
+
+        audioSource.resource = clickSound;
+        audioSource.Play();
+
+        while (audioSource.isPlaying == true) {
+            yield return null;
+        }
+
+        if (scene != null) {
+            SceneManager.LoadScene(scene);
+        }
+
         Application.Quit();
+        button.interactable = true; // set for demo purposes, not applicable in-game
     }
 
 
